67936ea9b3e43e602daa7c41a2decb4245ff07f5
Satoshi Scott Iwako <iwakoscott@gmail.com>
2018-07-23 15:18:27 -0700

    update ADD_USERNAME_BROWSER to ADD_USERNAME_TO_BROWSER

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,10 +1,10 @@
 import { addUsernameToBrowser } from "../utils/API";
 
-export const ADD_USERNAME_BROWSER = "ADD_USERNAME_BROWSER";
+export const ADD_USERNAME_TO_BROWSER = "ADD_USERNAME_TO_BROWSER";
 
 function addUsernameToStore(username) {
   return {
-    type: ADD_USERNAME_BROWSER,
+    type: ADD_USERNAME_TO_BROWSER,
     username
   };
 } // addUsernameToStore
